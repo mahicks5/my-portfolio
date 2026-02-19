67e4c90651339bc47e044732a7e612ada98e07cf
Maxwell <skate1011rocks@gmail.com>
2026-02-19 16:40:09 -0700

    fixed view on github button

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -59,7 +59,9 @@ export default function Projects() {
                                     {project.title}
                                 </h2>
 
-                                <a href="#" className="inline-flex items-center text-blue-400 font-medium hover:underline">
+                                <a
+                                    href={project.github}
+                                    className="inline-flex items-center text-blue-400 font-medium hover:underline">
                                     View on GitHub
                                     <svg className="w-5 h-5 ms-1 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 12H5m14 0-4 4m4-4-4-4" /></svg>
                                 </a>
