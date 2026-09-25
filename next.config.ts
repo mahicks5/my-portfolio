563553117d1d2b9f61fca663cfd0a78171c339c5
Maxwell <skate1011rocks@gmail.com>
2026-09-24 19:51:48 -0600

    Add redirects for resume, github, and linkedin

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      { source: "/resume", destination: "/Maxwell_Hicks_Resume.pdf", permanent: false },
+      { source: "/github", destination: "https://github.com/mahicks5", permanent: false },
+      { source: "/linkedin", destination: "https://www.linkedin.com/in/maxwell-h-2647622a4", permanent: false },
+    ];
+  },
 };
 
 export default nextConfig;
